b7899b7d2533746225fafce6258057424e2569f9
Chenguku <peasantrule@gmail.com>
2024-04-07 01:51:37 -0400

    Authentication Layout

--- final_project/src/views/Login.vue
+++ final_project/src/views/Login.vue
@@ -1,3 +1,43 @@
 <template>
     <h1>Login to Access your Transcripts</h1>
+    <div class="textinput">
+    <p>Username:</p>
+    <input type="text">
+    </div>
+    <div class="textinput">
+    <p>Password:</p>
+    <input type="text">
+    </div>
+    <button id="register">Login</button>
+    <div class="hasaccount">
+    <label>Don't have an account? </label><router-link to='/register'>Register</router-link>
+    </div>
 </template>
+
+
+<style>
+    .textinput {
+        margin-top: 10px;
+        font-size: 24px;
+        text-align:center;
+    }
+    input[type="text"]{
+        border: none;
+        border-radius: 3px;
+        padding:10px;
+        width: 40%;
+        font-size: 0.7em;
+    }
+    p{
+        margin-bottom: 0;
+    }
+    #register{
+        margin-top: 40px;
+        width: 42%;
+        font-size: 20px;
+    }
+    .hasaccount{
+        text-align: right;
+        margin-right: 30%;
+    }
+</style>
--- final_project/src/views/Register.vue
+++ final_project/src/views/Register.vue
@@ -1,3 +1,20 @@
 <template>
     <h1>Create an account</h1>
+    <div class="textinput">
+    <p>Username:</p>
+    <input type="text">
+    </div>
+    <div class="textinput">
+    <p>Password:</p>
+    <input type="text">
+    </div>
+    <button id="register">Create Account</button>
+    <div class="hasaccount">
+    <label>Already have an account? </label><router-link to='/login'>Login</router-link>
+    </div>
 </template>
+
+
+<style>
+    /* Refer to Login.vue */
+</style>
